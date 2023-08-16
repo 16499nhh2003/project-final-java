d87d3e49200b3519792e5d101d03dffd551f6288
Nguyễn Huy Hòa <52100797@student.tdtu.edu.vn>
2023-08-16 20:58:21 +0700

    up

---
+++ src/main/java/com/project/spring/model/User.java
@@ -0,0 +1,30 @@
+package com.project.spring.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phoneNumber;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Cart> carts;
+
+}
